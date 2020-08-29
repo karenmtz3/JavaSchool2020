eeda822d0cabfb03cd453169aad51124d2f70d55
karenmtz3 <karenmtz273@gmail.com>
2020-08-29 15:27:22 -0500

    Pruebas unitarias de PackageServiceImpl

--- src/test/java/com/shippingapp/shipping/services/impl/PackageServiceImplTest.java
+++ src/test/java/com/shippingapp/shipping/services/impl/PackageServiceImplTest.java
@@ -19,7 +19,10 @@ import java.util.List;
 @RunWith(MockitoJUnitRunner.class)
 public class PackageServiceImplTest {
 
-    private String message;
+    private String messageType;
+    private String messageSize;
+    private final static String PACKAGE_TYPE = "Box";
+
     private PackageServiceImpl packageService;
     private Connection connection;
     private AmqpTemplate rabbitTemplate;
@@ -29,72 +32,149 @@ public class PackageServiceImplTest {
         this.rabbitTemplate = Mockito.mock(AmqpTemplate.class);
         this.connection = Mockito.mock(Connection.class);
 
-        message = "{\"type\":\"packageType\"}";
+        messageType = "{\"type\":\"packageType\"}";
+        messageSize = "{\"type\":\"packageSizeByType\",\"packageType\":\"" + PACKAGE_TYPE + "\"}";
 
         packageService = new PackageServiceImpl(rabbitTemplate,connection);
     }
 
     @Test
-    public void getDescriptionsList_SuccessExpected() {
+    public void getDescriptionsForPackagesType_SuccessExpected() {
         String messageReceived  = "[{\"id\":3,\"description\":\"Envelop\",\"price\":5},{\"id\":4,\"" +
                 "description\":\"Box\",\"price\":12}]";
 
         List<String> responseExpected = Arrays.asList("Envelop", "Box");
 
         when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
-                        connection.getRoutingKey(), message)).thenReturn(messageReceived);
+                connection.getRoutingKey(), messageType)).thenReturn(messageReceived);
 
-        List<String> response = packageService.getPackageTypeDescriptions();
+        List<String> response = packageService.getDescriptionsForPackagesType();
 
         assertThat(response).isEqualTo(responseExpected);
     }
 
     @Test
-    public void getDescriptionsListWithMessageReceivedEmpty_ThenThrowPackageServiceException(){
-       String messageReceived = "";
+    public void getDescriptionsForPackagesTypeWithMessageReceivedEmpty_ThenThrowPackageServiceException(){
+        String messageReceived = "";
 
         when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
-                connection.getRoutingKey(), message)).thenReturn(messageReceived);
-
-       assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
-               () -> packageService.getPackageTypeDescriptions());
-    }
-
-    @Test
-    public void getDescriptionsListWithMessageReceivedNull_ThenThrowPackageServiceException(){
-        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
-                connection.getRoutingKey(), message)).thenReturn(null);
+                connection.getRoutingKey(), messageType)).thenReturn(messageReceived);
 
         assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
-                () -> packageService.getPackageTypeDescriptions());
+                () -> packageService.getDescriptionsForPackagesType());
     }
 
     @Test
-    public void getDescriptionsListWithAnyValueNullOfMessageReceived_SuccessExpected(){
+    public void getDescriptionsForPackagesTypeListWithMessageReceivedNull_ThenThrowPackageServiceException(){
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageType)).thenReturn(null);
+
+        assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
+                () -> packageService.getDescriptionsForPackagesType());
+    }
+
+    @Test
+    public void getDescriptionsForPackagesTypeWithAnyValueNullOfMessageReceived_SuccessExpected(){
         String messageReceived  = "[{\"id\":null,\"description\":\"Envelop\",\"price\":5},{\"id\":4,\"" +
                 "description\":\"Box\",\"price\":12}]";
 
         List<String> responseExpected = Collections.singletonList("Box");
 
         when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
-                connection.getRoutingKey(), message)).thenReturn(messageReceived);
+                connection.getRoutingKey(), messageType)).thenReturn(messageReceived);
 
-        List<String> response = packageService.getPackageTypeDescriptions();
+        List<String> response = packageService.getDescriptionsForPackagesType();
 
         assertThat(response).isEqualTo(responseExpected);
     }
 
     @Test
-    public void getDescriptionsListWithAnyValueEmptyOfMessageReceived_SuccessExpected(){
-        String messageReceived  = "[{\"id\":null,\"description\":\"\",\"price\":5},{\"id\":4,\"" +
+    public void getDescriptionsForPackagesTypeWithAnyValueEmptyOfMessageReceived_SuccessExpected(){
+        String messageReceived  = "[{\"id\":3,\"description\":\"\",\"price\":5},{\"id\":4,\"" +
                 "description\":\"Box\",\"price\":12}]";
 
         List<String> responseExpected = Collections.singletonList("Box");
 
         when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
-                connection.getRoutingKey(), message)).thenReturn(messageReceived);
+                connection.getRoutingKey(), messageType)).thenReturn(messageReceived);
 
-        List<String> response = packageService.getPackageTypeDescriptions();
+        List<String> response = packageService.getDescriptionsForPackagesType();
+
+        assertThat(response).isEqualTo(responseExpected);
+    }
+
+
+
+    @Test
+    public void getDescriptionsForPackageSize_SuccessExpected(){
+        String messageReceived = "[{\"id\":4,\"description\":\"Small\",\"priceFactor\":10}," +
+                "{\"id\":5,\"description\":\"Medium\",\"priceFactor\":25}," +
+                "{\"id\":6,\"description\":\"Large\",\"priceFactor\":50}]";
+
+        List<String> responseExpected = Arrays.asList("Small", "Medium", "Large");
+
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageSize)).thenReturn(messageReceived);
+
+        List<String> response = packageService.getDescriptionsForPackageSize(PACKAGE_TYPE);
+
+        assertThat(response).isEqualTo(responseExpected);
+    }
+
+    @Test
+    public void getDescriptionsForPackageSizeWithMessageReceivedEmpty_ThenThrowPackageServiceException(){
+        String messageReceived = "";
+
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageSize)).thenReturn(messageReceived);
+
+        assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
+                () -> packageService.getDescriptionsForPackageSize(PACKAGE_TYPE));
+    }
+
+    @Test
+    public void getDescriptionsForPackageSizeListWithMessageReceivedNull_ThenThrowPackageServiceException(){
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageSize)).thenReturn(null);
+
+        assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
+                () -> packageService.getDescriptionsForPackageSize(PACKAGE_TYPE));
+    }
+
+    @Test
+    public void getDescriptionsForPackageSizeWithPackageTypeEmpty_ThenThrowPackageServiceException(){
+        assertThatExceptionOfType(PackageServiceException.class).isThrownBy(
+                () -> packageService.getDescriptionsForPackageSize(" "));
+    }
+
+    @Test
+    public void getDescriptionsForPackageSizeWithAnyValueNullOfMessageReceived_SuccessExpected(){
+        String messageReceived = "[{\"id\":null,\"description\":\"Small\",\"priceFactor\":10}," +
+                "{\"id\":5,\"description\":\"Medium\",\"priceFactor\":25}," +
+                "{\"id\":null,\"description\":\"Large\",\"priceFactor\":50}]";
+
+        List<String> responseExpected = Collections.singletonList("Medium");
+
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageSize)).thenReturn(messageReceived);
+
+        List<String> response = packageService.getDescriptionsForPackageSize(PACKAGE_TYPE);
+
+        assertThat(response).isEqualTo(responseExpected);
+    }
+
+    @Test
+    public void getDescriptionsForPackageSizeWithAnyValueEmptyOfMessageReceived_SuccessExpected(){
+        String messageReceived = "[{\"id\":4,\"description\":\"\",\"priceFactor\":10}," +
+                "{\"id\":5,\"description\":\"Medium\",\"priceFactor\":25}," +
+                "{\"id\":6,\"description\":\"Large\",\"priceFactor\":50}]";
+
+        List<String> responseExpected = Arrays.asList("Medium", "Large");
+
+        when(rabbitTemplate.convertSendAndReceive(connection.getExchange(),
+                connection.getRoutingKey(), messageSize)).thenReturn(messageReceived);
+
+        List<String> response = packageService.getDescriptionsForPackageSize(PACKAGE_TYPE);
 
         assertThat(response).isEqualTo(responseExpected);
     }
